414db63cac133cb7b36dbeddf1abc5fb43db3d23
GOPIKA MP <gopikaramesh360@gmail.com>
2024-06-15 21:38:52 +0530

    updated

---
+++ models/bus.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose")
+const schema = mongoose.Schema(
+    {
+        "busname":String,
+        "route":String,
+        "busno":String,
+        "drivername":String
+    }
+)
+
+let busmodel = mongoose.model("buses",schema)
+module.exports = {busmodel}
